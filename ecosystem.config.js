cc70f4f7db2db4c3b20d73ad589cde4a60546469
Devin George <devin.k.george@gmail.com>
2019-11-05 14:12:54 -0600

    Update AWS instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-34-214-20-208.us-west-2.compute.amazonaws.com',
+      host: 'ec2-18-236-136-56.us-west-2.compute.amazonaws.com',
       key: '~/.ssh/Amishon-Proxy.pem',
       ref: 'origin/master',
       repo: 'git@github.com:Amishon/Devin-Proxy.git',
